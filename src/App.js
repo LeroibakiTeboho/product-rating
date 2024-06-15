a1b7895737e756a21f57a1aa75db249345368b00
Leroibaki Teboho <leroibakiteboho@gmail.com>
2024-06-15 15:47:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import RatingStars from "./component/RatingStars";
+
+
+function App() {
+  return (
+    <div className="App">
+      <RatingStars noOfStars={7}/>
+    </div>
+  );
+}
+
+export default App;
